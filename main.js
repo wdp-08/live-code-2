bae903df7a2d66b6bd5bbc30b1a2c5d473ac19cf
Rivaldi Alwi <rivaldialwi42@gmail.com>
2023-03-23 21:34:39 +0700

    Update js live code 2

---
+++ main.js
@@ -0,0 +1,34 @@
+let biodata = {
+    nama_depan : "Rivaldi",
+    nama_belakang : "Alwi",
+    hobi : ["Bermain Games", "Nonton Film"],
+    angka_fav : 3,
+    kacamata : true
+}
+
+// console.log(biodata);
+
+// console.log(biodata.nama_depan + " " + biodata.nama_belakang);
+
+// biodata.angka_fav = 8 ;
+// console.log(biodata.angka_fav);
+
+// biodata.hobi.push("coding")
+// console.log(biodata.hobi);
+
+// biodata.lulusan = "Hacktiv8"
+// console.log(biodata.lulusan);
+
+// for (let index = 0; index < biodata.hobi.length; index++) {
+//     console.log(biodata.hobi[index]);
+// }
+
+// let keys = Object.keys(biodata)
+// let values = Object.values(biodata)
+
+// console.log(keys);
+// console.log(values);
+
+// for (let prop in biodata) {
+//     console.log(prop + ": " + biodata[prop]);
+// }
