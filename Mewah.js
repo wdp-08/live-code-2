dd87d6629176ca86c9d26cfa606d69dd09e678cc
Mewah Aprilia Susyanti <111202012801@mhs.dinus.ac.id>
2023-03-23 21:33:23 +0700

    Biodata

--- Mewah.js
+++ Mewah.js
@@ -7,17 +7,21 @@ let Me = {
   isWearingGlasses: true,
 };
 
+// Untuk Mencetak Nama Lengkap
 console.log(`Nama saya adalah ${Me.NamaDepan} ${Me.NamaTengah} ${Me.NamaBelakang}`);
 
 Me.NomerFavorit = 8;
 
+// Untuk Mencetak Angka Favorit
 console.log(`Angka favorit berubah menjadi ${Me.NomerFavorit}`);
 
 Me.lulusan = "SMAN 3 SLawi";
 Me["lulusan"] = "SMAN 3 SLawi";
 
+// Untuk Mencetak Lulusan
 console.log(`Saya adalah lulusan ${Me.lulusan}`);
 
+// Untuk Mencetak Hobi Saya
 console.log("hobi saya adalah");
 Me.Hobi.forEach((hobby) => {
   console.log(`- ${hobby}`);
