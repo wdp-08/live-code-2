4bfc8df7d6d0f0feed28be8041945ac78bc15b63
AdamBagusP <adampdn01.ab@gmail.com>
2023-03-23 21:40:40 +0700

    add index edit script

---
+++ script2.js
@@ -0,0 +1,14 @@
+let bio = {
+    fname: "Adam",
+    lname: "Bagus",
+    hobi: ["gaming", "nonton bola"],
+    favNum: 1,
+    useGlasses : false
+}
+
+// console.log(bio);
+console.log(bio.fname, bio.lname);
+bio.hobi[2] = "coding"
+bio.favNum = 8 
+// bio.push =  lulusan : "Hacktiv8"
+console.log(bio);
