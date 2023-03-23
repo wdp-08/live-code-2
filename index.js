ad2224aeee59798d1b635b72abddde153025647e
ararizqia <ararizqiaswin@gmail.com>
2023-03-23 21:29:06 +0700

    membuat array saya

---
+++ index.js
@@ -0,0 +1,10 @@
+let saya = [
+    {
+        "nama_depan" : "Zahra Rizqia",
+        "nama_belakang" : "Aswin",
+        "hobi" : ["Baca Manga","Makan Nasi","Minum Air"],
+        "angka_fav" : 7,
+        "memakai_kacamata" : false
+    }
+]
+console.log(saya);
