ffab6378e2f8e805ec11451a6ad81529f3912a63
Azkaashla <ursilaazka13@gmail.com>
2023-03-23 21:47:48 +0700

    create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+// objek 
+
+function objek() {
+    var identity = {
+        nama depan:"azka"
+        nama belakang:"ursila"
+        hobi:"touring"
+        angka favorit:"3"
+        warna favorit:"beige"
+    }
+    
+    // cetak nama
+    console.log(identity.nama belakang);
+
+    // ubah angka
+    identity.angka favorit = 8;
+    console.log(identity);
+
+    // ubah hobi
+    identity.hobi = coding;
+    console.log(identity);
+
+    // ubah nama
+    identity.nama depan = ardian;
+    console.log(identity);
+
+    // tambah value
+    identity.lulusan = Hacktiv8;
+    console.log(identity);
+
+    // loop
+    for(let i = 0 <5; i++) {
+        document.write(identity + i + identity);
+    }
+}
